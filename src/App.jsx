d280c80e5aa34d562b3604a444a893fb5f38de66
Filipe Avanzzo <avanzzodev@gmail.com>
2023-10-21 20:55:14 -0300

    logic and custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Animal from "./components/Animal";
+import useAnimalSearch from "./hooks/useAnimalSearch";
 function App() {
-  return <></>;
+  const { animals, search } = useAnimalSearch();
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
